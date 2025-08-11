17a6987944edbbeff6af67388ca519237537021a
Daniel Mezker <dmezker06@gmail.com>
2025-08-11 14:36:10 +0200

    Test 15

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -5,19 +5,20 @@ const confirmPassword = document.getElementById('confirmPassword');
 
 function check() {
 
-    // if(username.length < 4) {
-    //     alert("Benutzername muss mindestens 4 Zeichen enthalten.");
-    //     return;
-    // }
-
-    // if(password.length < 8) {
-    //     alert("Password muss mindestens 8 Zeichen enthalten.");
-    //     return;
-    // }
-    alert(username.value + ", " + password.value);
-
-    if(password.value != confirmPassword.value) {
+    if(username.value.length < 4) {
+        alert("Benutzername muss mindestens 4 Zeichen enthalten.");
+        return;
+    }
+    else if(password.value.length < 8) {
+        alert("Password muss mindestens 8 Zeichen enthalten.");
+        return;
+    }
+    else if(password.value != confirmPassword.value) {
         alert("Passwörter stimmen nicht überein.");
         return;
+    }
+    else {
+        alert("Erfolg");
+        window.location.href = "http://10.3.15.52/pages/to_do_lists.html";
     }
 }
