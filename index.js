c94778c1a80fe3a33b3cff44132e507a0ff4ffb2
Daniel Mezker <dmezker06@gmail.com>
2025-08-11 08:47:10 +0200

    Trying to setup Views 3

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('To-Do-List App läuft! Test 1 erfolgreich abgeschlossen');
-});
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send('To-Do-List App läuft! Test 1 erfolgreich abgeschlossen');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf Port ${PORT}`);
