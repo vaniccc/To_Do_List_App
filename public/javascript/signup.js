3f682719dc8748683a1e6539e1d66b6955c5efa1
Daniel Mezker <dmezker06@gmail.com>
2025-08-11 14:22:05 +0200

    Test 10

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -5,15 +5,15 @@ const confirmPassword = document.getElementById('confirmPassword').value;
 
 function check() {
 
-    if(username.length < 4) {
-        alert("Benutzername muss mindestens 4 Zeichen enthalten.");
-        return;
-    }
+    // if(username.length < 4) {
+    //     alert("Benutzername muss mindestens 4 Zeichen enthalten.");
+    //     return;
+    // }
 
-    if(password.length < 8) {
-        alert("Password muss mindestens 8 Zeichen enthalten.");
-        return;
-    }
+    // if(password.length < 8) {
+    //     alert("Password muss mindestens 8 Zeichen enthalten.");
+    //     return;
+    // }
 
     if(password !== confirmPassword) {
         alert("Passwörter stimmen nicht überein.");
