0f8fd6886a449a75df62b8eeee643cebb2cdb5c2
Daniel Mezker <dmezker06@gmail.com>
2025-08-11 14:20:44 +0200

    Test 9

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,11 +1,22 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username').value
-const password = document.getElementById('password').value
-const confirmPassword = document.getElementById('confirmPassword').value
+const form = document.getElementById('form');
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+const confirmPassword = document.getElementById('confirmPassword').value;
 
-function check(form) {
+function check() {
+
+    if(username.length < 4) {
+        alert("Benutzername muss mindestens 4 Zeichen enthalten.");
+        return;
+    }
+
+    if(password.length < 8) {
+        alert("Password muss mindestens 8 Zeichen enthalten.");
+        return;
+    }
+
     if(password !== confirmPassword) {
-        alert("Passwörter stimmen nicht überein.")
+        alert("Passwörter stimmen nicht überein.");
         return;
     }
 }
