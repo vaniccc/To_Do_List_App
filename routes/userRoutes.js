82e632d452223163063ed322724e791515363317
Daniel Mezker <dmezker06@gmail.com>
2025-08-12 11:40:35 +0200

    Test 44

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -36,7 +36,8 @@ router.post('/register', async (req, res) => {
         
         const hashedPassword = await bcrypt.hash(password, 10); 
 
-        console.log(username.value + hashedPassword.value)
+        console.log(username.value);
+        console.log(hashedPassword);
 
         // await pool.query(
         //     `INSERT INTO users (username, password_hash) VALUES ('${username}', '${hashedPassword}');`);
