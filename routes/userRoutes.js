68f50b7a552feca9997f77a5226af80edf735a4b
Daniel Mezker <dmezker06@gmail.com>
2025-08-12 10:40:42 +0200

    Test 32

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/db')
+const pool = require('../config/db');
 const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
