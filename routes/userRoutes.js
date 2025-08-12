7b1ee09163a5e3276db2803466b0fa4a1043cf9a
Daniel Mezker <dmezker06@gmail.com>
2025-08-12 11:32:47 +0200

    Test 41

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,8 +20,9 @@ router.post('/register', async (req, res) => {
         }
 
     try {
-       const userCheck = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-        // const userCheck = await pool.query(`SELECT * FROM users WHERE username = '${username}'`);        
+        try{
+            // const userCheck = await pool.query(`SELECT * FROM users WHERE username = '${username}'`);        
+        const userCheck = await pool.query('SELECT * FROM users WHERE username = $1', [username]);       
         console.log("UserCheck 1: ");
         console.log(userCheck.rows);
 
@@ -29,27 +30,33 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: 'Benutzername ist bereits vergeben.'});
         }
 
+        }catch (err) {
+            console.error(err);
+        }
         
         const hashedPassword = await bcrypt.hash(password, 10); 
 
         // await pool.query(
         //     `INSERT INTO users (username, password_hash) VALUES ('${username}', '${hashedPassword}');`);
 
-        const result = await pool.query(
+        try {
+      const result = await pool.query(
             'INSERT INTO users (username, password_hash) VALUES ($1, $2)',
             [username, hashedPassword]
         );
         console.log("result: ");
-        console.log(result.rows);   
+        console.log(result.rows); 
+        } catch (erro) {
+            console.error(erro);
+        }
+
+          
 
         const userCheck2 = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
         // const userCheck = await pool.query(`SELECT * FROM users WHERE username = '${username}'`);        
         console.log("UserCheck 2: ");
         console.log(userCheck2.rows);
 
-        if(userCheck2.rows.length > 0) {
-            return res.status(400).json({ message: 'Benutzername ist bereits vergeben.'});
-        }
 
         res.status(201).json({ message: "Benutzer erfolgreich registiert"});
     }
